046e3aa04dc9af1a810010e678f133da4b2a3ffe
Emil <me@emilvinu.de>
2024-09-01 21:20:44 +0200

    modified deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 cd backmind
-/home/emil/.nvm/versions/node/v20.12.1/bin/pm2 stop backmind
+pm2 stop backmind
 git pull
-/home/emil/.nvm/versions/node/v20.12.1/bin/npm i
-/home/emil/.nvm/versions/node/v20.12.1/bin/npx tsc
-/home/emil/.nvm/versions/node/v20.12.1/bin/pm2 start dist/index.js -i 1 --name backmind
+npm i
+npx tsc
+pm2 start dist/index.js -i 1 --name backmind

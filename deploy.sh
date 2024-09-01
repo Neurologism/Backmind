30f2e7a4e6548664daaeba03a13c2490f5142353
Emil <me@emilvinu.de>
2024-09-01 21:31:34 +0200

    fixed deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+PATH="/home/emil/.nvm/versions/node/v20.12.1/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/snap/bin"
 cd backmind
 pm2 stop backmind
 git pull

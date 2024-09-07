cdfb1ad938c5d3781b372c1322a178128abab642
Emil <me@emilvinu.de>
2024-09-07 21:50:46 +0200

    added example scripts

---
+++ examples/login.cjs
@@ -0,0 +1,23 @@
+const payload = {
+  user: {
+    email: 'max@mustermann.net',
+    // brainet_tag: 'randomuser1234',
+    plain_password: '1234',
+  },
+};
+
+fetch('http://localhost:3000/api/auth/login', {
+  method: 'POST',
+  headers: {
+    'Content-Type': 'application/json',
+  },
+  body: JSON.stringify(payload),
+})
+  .then((response) => response.json())
+  .then((data) => console.log(data));
+
+/*
+{
+  token: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NmRjYTk1ZjEzYTNjNGZmMTg3Y2M0NzQiLCJpYXQiOjE3MjU3Mzg1MTcsImV4cCI6MTcyNTgyNDkxN30.wo6mTQItZhJ4k_EuoK22dO1_0yRFVm1tuZgypTzFZSw'
+}
+*/

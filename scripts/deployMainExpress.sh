dbff5bf4cfd7bf2e81d2044d44576d2872a194f8
Emil <me@emilvinu.de>
2024-09-18 12:34:35 +0200

    updated gh actions workflows for ocean servers

---
+++ scripts/deployMainExpress.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+PATH="/usr/local/bin:/usr/bin:/bin:/usr/local/games:/usr/games"
+cd /home/brainet/backmind
+pm2 delete backmind-server
+git reset --hard
+git clean -f
+git checkout main
+git pull
+npm i
+npm run build
+npm run server-start

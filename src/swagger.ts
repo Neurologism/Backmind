f116c0af315e23e0eb31e15ae7d62af0474d0641
Emil <me@emilvinu.de>
2024-09-18 21:42:17 +0200

    added projectDelete endpoint to swagger docs

--- src/swagger.ts
+++ src/swagger.ts
@@ -515,13 +515,59 @@ const swaggerDefinition = {
     },
     '/api/project/delete': {
       post: {
-        summary: 'in development',
+        summary: 'Delete a project. ',
         security: [
           {
             bearerAuth: [],
           },
         ],
-        deprecated: true,
+        description:
+          'To delete a project, you have to be logged in and provide your auth token. Furthermore, you have to specify the project id.',
+        requestBody: {
+          required: true,
+          content: {
+            'application/json': {
+              schema: {
+                type: 'object',
+                properties: {
+                  project: {
+                    _id: 'object',
+                    properties: {
+                      _id: {
+                        type: 'string',
+                        example: '029iiu73h4t42e5t29io0u3h45t',
+                      },
+                    },
+                  },
+                },
+              },
+            },
+          },
+        },
+        responses: {
+          '200': {
+            description: 'Project deleted successfully.',
+            content: {
+              'application/json': {
+                schema: {
+                  type: 'object',
+                  properties: {
+                    msg: {
+                      type: 'string',
+                      example: 'Project deleted successfully.',
+                    },
+                  },
+                },
+              },
+            },
+          },
+          '400': {
+            description: 'Invalid input.',
+          },
+          '500': {
+            description: 'Internal server error.',
+          },
+        },
       },
     },
     '/api/project/search': {

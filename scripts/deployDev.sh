704eae2e38172c35c74f7cafbbb6dc0ab15b0d1a
Emil <me@emilvinu.de>
2024-10-07 18:42:59 +0200

    updated gh actions for new dev server

--- scripts/deployDev.sh
+++ scripts/deployDev.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-PATH="/home/emil/.nvm/versions/node/v20.12.1/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/snap/bin"
-cd backmind
+PATH="/home/gh-actions/.nvm/versions/node/v20.18.0/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin"
+cd Backmind
 pm2 stop backmind-server
 pm2 delete backmind-server
 git reset --hard

c47776b03ac5c7ce3b0629a37d6ea3ab07756a6b
Emil <me@emilvinu.de>
2024-10-23 13:58:45 +0200

    added mongoose project schema and model

---
+++ src/mongooseSchemas/projectSchema.ts
@@ -0,0 +1,16 @@
+import mongoose from 'mongoose';
+
+const mongooseProjectSchema = new mongoose.Schema({
+  name: String,
+  description: String,
+  owner_id: mongoose.Types.ObjectId,
+  contributors: [mongoose.Types.ObjectId],
+  visibility: { type: String, enum: ['public', 'private'], required: true },
+  created_on: Number,
+  last_edited: Number,
+  camera_position: [Number],
+  components: Object,
+  models: [mongoose.Types.ObjectId]
+});
+
+export const ProjectModel = mongoose.model('Project', mongooseProjectSchema);

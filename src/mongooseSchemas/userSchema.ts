98c6358cecf80ca046c7ec63ddaaa984a24348f6
Emil <me@emilvinu.de>
2024-10-23 13:58:59 +0200

    added mongoose user schema and model

---
+++ src/mongooseSchemas/userSchema.ts
@@ -0,0 +1,17 @@
+import mongoose from 'mongoose';
+
+const mongooseUserSchema = new mongoose.Schema({
+  email: String,
+  about_you: String,
+  displayname: String,
+  brainet_tag: String,
+  password_hash: String,
+  date_of_birth: Number,
+  visibility: { type: String, enum: ['public', 'private'] },
+  created_on: Number,
+  project_ids: [mongoose.Types.ObjectId],
+  follower_ids: [mongoose.Types.ObjectId],
+  following_ids: [mongoose.Types.ObjectId],
+});
+
+export const UserModel = mongoose.model('User', mongooseUserSchema);

7c9a4fc58508c1e99384aba18020db421b5ff3e8
Emil <me@emilvinu.de>
2024-11-12 15:06:52 +0100

    removed components schema validation

--- src/schemas/componentsSchemas.ts
+++ src/schemas/componentsSchemas.ts
@@ -1,35 +1,37 @@
 import { z } from 'zod';
 
-export const componentParametersSchema = z.array(
-  z
-    .object({
-      type: z.enum(['parameter', 'object']),
-      value: z.any(),
-      parameters: z.lazy((): any => componentParametersSchema).optional(),
-    })
-    .strict()
-);
+// export const componentParametersSchema = z.array(
+//   z
+//     .object({
+//       type: z.enum(['parameter', 'object']),
+//       value: z.any(),
+//       parameters: z.lazy((): any => componentParametersSchema).optional(),
+//     })
+//     .strict()
+// );
 
-export const operationSchema = z
-  .object({
-    type: z.string(),
-    method: z.string(),
-    uid: z.string(),
-    args: z.object({}),
-  })
-  .strict();
+// export const operationSchema = z
+//   .object({
+//     type: z.string(),
+//     method: z.string(),
+//     uid: z.string(),
+//     args: z.object({}),
+//   })
+//   .strict();
 
-export const linkSchema = z
-  .object({
-    uid: z.string(),
-    source: z.string(),
-    target: z.string(),
-  })
-  .strict();
+// export const linkSchema = z
+//   .object({
+//     uid: z.string(),
+//     source: z.string(),
+//     target: z.string(),
+//   })
+//   .strict();
 
-export const componentsSchema = z
-  .object({
-    operations: z.array(operationSchema),
-    links: z.array(linkSchema),
-  })
-  .strict();
+// export const componentsSchema = z
+//   .object({
+//     operations: z.array(operationSchema),
+//     links: z.array(linkSchema),
+//   })
+//   .strict();
+
+export const componentsSchema = z.object({});

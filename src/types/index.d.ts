c368fb3bd6e84886c6fb1b65204156b33ea10bfe
Emil <me@emilvinu.de>
2024-11-23 15:11:30 +0100

    removed obsolete type script user and project and requestExplicit types

--- src/types/index.d.ts
+++ src/types/index.d.ts
@@ -16,15 +16,3 @@ declare module 'express-serve-static-core' {
     logger: Logger;
   }
 }
-
-export interface RequestExplicit extends Request {
-  user_id: ObjectId | null;
-  db: Db;
-  dbUsers: Collection;
-  dbProjects: Collection;
-  dbTrainingQueue: Collection;
-  dbModels: Collection;
-}
-
-export * from './user';
-export * from './project';

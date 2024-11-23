f202c45f55a8f216b63e5ae5c2713a47c8b1742a
Emil <me@emilvinu.de>
2024-11-23 14:45:45 +0100

    migrated connectToDatabase utility script to mongoose

--- src/utility/connectToDatabase.ts
+++ src/utility/connectToDatabase.ts
@@ -1,40 +1,21 @@
-import { MongoClient, Db } from 'mongodb';
-import { arraysEqual } from './arraysEqual';
+import mongoose from 'mongoose';
+import { ProjectModel } from '../mongooseSchemas/projectSchema';
+import { QueueItemModel } from '../mongooseSchemas/queueItemSchema';
+import { TaskModel } from '../mongooseSchemas/taskSchema';
+import { UserModel } from '../mongooseSchemas/userSchema';
 
-const collections = ['users', 'projects', 'training_queue', 'models'];
-let connected = false;
-let db: Db;
-let client: MongoClient;
-
-export const connectToDatabase = async (): Promise<Db> => {
-  if (connected) {
-    return db;
-  }
+export const connectToDatabase = async () => {
   console.log('Trying to connect to the database...');
-  client = new MongoClient(process.env.MONGO_URI as string);
-  await client.connect();
-  db = client.db(process.env.DB_NAME);
+  mongoose.connect(process.env.MONGO_URI as string);
 
-  let resetDb = process.env.RESET_DB === 'true';
-  if ((await db.listCollections().toArray()).length !== collections.length) {
-    console.log('Resetting databse due to missing collections...');
-    resetDb = true;
-  }
-  if (resetDb) {
+  if (process.env.RESET_DB === 'true') {
     console.log('Resetting database... ');
-    await db.dropDatabase();
-    for (const collectionName of collections) {
-      await db.createCollection(collectionName);
-    }
+    ProjectModel.deleteMany({});
+    QueueItemModel.deleteMany({});
+    TaskModel.deleteMany({});
+    UserModel.deleteMany({});
     console.log('Database reset successful');
   }
 
   console.log(`Connected to database: ${process.env.DB_NAME}`);
-  connected = true;
-  return db;
-};
-
-export const disconnectFromDatabase = async () => {
-  await client.close();
-  connected = false;
 };

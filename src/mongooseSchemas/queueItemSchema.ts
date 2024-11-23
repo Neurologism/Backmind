7d8c6a7ed17f915f6a0249c918d76ec446670ccb
Emil <me@emilvinu.de>
2024-11-23 14:52:17 +0100

    added queue_position entry to mongoose queueItem schema

--- src/mongooseSchemas/queueItemSchema.ts
+++ src/mongooseSchemas/queueItemSchema.ts
@@ -2,6 +2,7 @@ import mongoose from 'mongoose';
 
 const mongooseQueueItemSchema = new mongoose.Schema({
   model_id: mongoose.Types.ObjectId,
+  queue_position: Number
 });
 
 export const QueueItemModel = mongoose.model(

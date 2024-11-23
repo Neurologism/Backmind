6132b187b00dde0b00ec087bd4454c53f6c26a7e
Emil <me@emilvinu.de>
2024-11-23 17:53:30 +0100

    migrated controllers to mongoose

--- src/controllers/projectModelController.ts
+++ src/controllers/projectModelController.ts
@@ -1,8 +1,11 @@
 import { Request, Response } from 'express';
+import { ProjectModel } from '../mongooseSchemas/projectSchema';
+import { QueueItemModel } from '../mongooseSchemas/queueItemSchema';
+import { TaskModel } from '../mongooseSchemas/taskSchema';
 
 export const modelStartTraining = async (req: Request, res: Response) => {
   if (
-    (await req.dbTrainingQueue!.countDocuments()) >
+    (await QueueItemModel.countDocuments()) >
     Number(process.env.MAX_TRAINING_QUEUE_LENGTH)
   ) {
     return res
@@ -10,7 +13,7 @@ export const modelStartTraining = async (req: Request, res: Response) => {
       .send({ msg: 'Training queue is full. Try again later.' });
   }
 
-  const model = {
+  const insertResult = await new TaskModel({
     status: 'queued',
     output: [],
     task: req.project.components,
@@ -19,14 +22,15 @@ export const modelStartTraining = async (req: Request, res: Response) => {
     started_at: null,
     finished_at: null,
     project_id: req.project._id,
-  };
-
-  const insertResult = await req.dbModels!.insertOne(model);
-  const modelId = insertResult.insertedId;
-  await req.dbTrainingQueue!.insertOne({ model_id: modelId });
-  await req.dbProjects!.updateOne({ _id: req.project._id }, {
-    $push: { models: modelId },
-  } as any);
+  });
+  const modelId = insertResult._id;
+  await new QueueItemModel({ model_id: modelId });
+  await ProjectModel.updateOne(
+    { _id: req.project._id },
+    {
+      $push: { models: modelId },
+    }
+  );
 
   res
     .status(200)
@@ -37,10 +41,10 @@ export const modelStopTraining = async (req: Request, res: Response) => {
   const model = req.body.model;
 
   if (model.status === 'queued') {
-    await req.dbTrainingQueue!.deleteOne({ model_id: model!._id });
+    await QueueItemModel.deleteOne({ model_id: model!._id });
   }
 
-  await req.dbModels!.updateOne(
+  await TaskModel.updateOne(
     { _id: model._id },
     {
       $set: {

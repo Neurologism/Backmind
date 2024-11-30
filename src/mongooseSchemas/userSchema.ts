e8b4fddc3297acfff3f525e14ab44ebd7dd8508b
Emil <me@emilvinu.de>
2024-11-30 12:59:58 +0100

    changed email saving and added phone saving attributes to mongoose user schema

--- src/mongooseSchemas/userSchema.ts
+++ src/mongooseSchemas/userSchema.ts
@@ -1,28 +1,60 @@
 import mongoose from 'mongoose';
 
-const mongooseUserSchema = new mongoose.Schema({
-  email: String,
-  about_you: String,
-  displayname: String,
-  brainet_tag: String,
-  password_hash: String,
-  date_of_birth: Number,
-  visibility: { type: String, enum: ['public', 'private'] },
-  last_edited: Date,
-  created_on: Date,
-  project_ids: [mongoose.Types.ObjectId],
-  follower_ids: [mongoose.Types.ObjectId],
-  following_ids: [mongoose.Types.ObjectId],
-  pfp_path: String,
-});
-
-mongooseUserSchema.set('toJSON', {
-  transform: (doc, ret) => {
-    delete ret.__v;
-    delete ret.password_hash;
-    delete ret.pfp_path;
-    return ret;
+export const mongooseUserSchema = new mongoose.Schema({
+  emails: {
+    type: [
+      {
+        type: {
+          emailType: {
+            type: String,
+            enum: ['primary', 'secondary'],
+            required: true,
+          },
+          address: { type: String, required: true },
+          verified: { type: Boolean, default: false },
+          verificationToken: { type: String },
+          dateVerificationSent: { type: Date },
+          dateAdded: { type: Date, default: new Date() },
+          dateVerified: { type: Date, required: false },
+        },
+      },
+    ],
+    required: true,
   },
+  phone: {
+    type: {
+      number: { type: String },
+      verified: { type: Boolean, default: false },
+      verificationCode: { type: String },
+      dateVerificationSent: { type: Date },
+      dateAdded: { type: Date },
+      dateVerified: { type: Date },
+    },
+    required: false,
+  },
+  about_you: { type: String, default: '' },
+  displayname: { type: String, default: '' },
+  brainet_tag: { type: String },
+  password_hash: { type: String, required: true },
+  date_of_birth: { type: Date, default: new Date() },
+  visibility: {
+    type: String,
+    enum: ['public', 'private'],
+    default: 'public',
+    required: true,
+  },
+  last_edited: { type: Date, default: new Date() },
+  created_on: { type: Date, default: new Date() },
+  project_ids: { type: [mongoose.Types.ObjectId], default: [] },
+  follower_ids: {
+    type: [mongoose.Types.ObjectId],
+    default: [],
+  },
+  following_ids: {
+    type: [mongoose.Types.ObjectId],
+    default: [],
+  },
+  pfp_path: { type: String, default: '' },
 });
 
 mongooseUserSchema.pre('save', function (next) {

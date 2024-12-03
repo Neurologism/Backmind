b8f0131eb9c548a1fcee2fb171f448f3aa8fd67c
Emil <me@emilvinu.de>
2024-12-03 21:27:22 +0100

    added tutorialId and tutorialStep attributes to project mongoose schema and a few minor changes

--- src/mongooseSchemas/projectSchema.ts
+++ src/mongooseSchemas/projectSchema.ts
@@ -1,8 +1,15 @@
 import mongoose from 'mongoose';
+import { initComponents } from '../utility/initComponents';
 
 const mongooseProjectSchema = new mongoose.Schema({
   name: { type: String, required: true },
-  description: { type: String, required: true, default: '' },
+  description: {
+    type: String,
+    required: function () {
+      return typeof (this as any).pfpPath === 'string';
+    },
+    default: '',
+  },
   ownerId: { type: mongoose.Types.ObjectId, required: true },
   contributors: {
     type: [mongoose.Types.ObjectId],
@@ -15,10 +22,12 @@ const mongooseProjectSchema = new mongoose.Schema({
   components: {
     type: mongoose.Schema.Types.Mixed,
     required: true,
-    default: {},
+    default: initComponents(),
   },
   models: { type: [mongoose.Types.ObjectId], required: true, default: [] },
-  isTutorialProject: { type: Boolean, required: true, default: false }, // mean that this tutorial is used to save the current state of a user following a tutorial; if true, should not be shown in the users project list
+  isTutorialProject: { type: Boolean, required: true, default: false },
+  tutorialId: mongoose.Types.ObjectId,
+  tutorialStep: { type: Number, required: false },
 });
 
 mongooseProjectSchema.pre('save', function (next) {

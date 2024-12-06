68d6b07604dc3ba3b424d4042adaa145ecce58c7
Emil <me@emilvinu.de>
2024-12-06 20:32:56 +0100

    updated deploy scripts to update in-memory pm2

--- scripts/deployDevExpress.sh
+++ scripts/deployDevExpress.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 PATH="/home/gh-actions/.nvm/versions/node/v20.18.0/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin"
 cd /home/gh-actions/Backmind
+npm install pm2@latest -g
+pm2 update
 pm2 delete backmind-server
 git reset --hard
 git clean -fd
--- scripts/deployMainExpress.sh
+++ scripts/deployMainExpress.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 PATH="/usr/local/bin:/usr/bin:/bin:/usr/local/games:/usr/games"
 cd /home/brainet/Backmind
+npm install pm2@latest -g
+pm2 update
 pm2 delete backmind-server
 git reset --hard
 git clean -fd

e757e4320820b34d6e1a091e17d28a6d4bb37fc7
Servant-of-Scietia <samsun2006@icloud.com>
2024-12-20 15:52:20 +0100

    fix bug

--- src/middleware/accessProjectMiddleware.ts
+++ src/middleware/accessProjectMiddleware.ts
@@ -33,8 +33,8 @@ export const accessProjectMiddleware = async (
         if (contributor._id.toString() === req.userId?.toString()) {
           return true;
         }
-        return false;
       }
+      return false;
     })();
   if (!canUpdateProject) {
     return res.status(404).json({

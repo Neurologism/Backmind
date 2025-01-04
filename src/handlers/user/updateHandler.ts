4a7eee34c9040cd19e5bc9a560bd9df9311c0e7c
Servant-of-Scietia <samsun2006@icloud.com>
2025-01-04 20:16:29 +0100

    update user endpoints to optionally update location company and pronouns, also enable change of primary email if not verified

--- src/handlers/user/updateHandler.ts
+++ src/handlers/user/updateHandler.ts
@@ -1,6 +1,9 @@
 import { Request, Response } from 'express';
 import bcrypt from 'bcrypt';
 import { UserModel } from '../../mongooseSchemas/userSchema';
+import sgMail from '@sendgrid/mail';
+import crypto from 'crypto';
+import { URL } from 'url';
 
 export const updateHandler = async (req: Request, res: Response) => {
   if (req.userId === undefined) {
@@ -31,6 +34,59 @@ export const updateHandler = async (req: Request, res: Response) => {
       );
       delete req.body.user.newPassword;
     }
+  }
+
+  if (
+    !user.emails.some(
+      (email) => email.emailType === 'primary' && email.verified
+    )
+  ) {
+    if (req.body.user.primaryEmail !== undefined) {
+      user.emails.pull({ emailType: 'primary' });
+
+      const mailVerificationToken = crypto.randomBytes(32).toString('hex');
+      const verificationLink = new URL(
+        `/api/auth/verify-email?token=${mailVerificationToken}`,
+        process.env.BACKMIND_HOSTNAME as string
+      ).toString();
+
+      const sendVerification = !Boolean(process.env.VERIFY_ALL_EMAILS);
+      if (sendVerification) {
+        try {
+          await sgMail.send({
+            to: req.body.user.primaryEmail,
+            from: 'no-reply@whitemind.net',
+            subject: 'Verify your Email',
+            text: `Verify your email address \nYou need to verify your email address to create your account. Click the link below to verify your email address. The link will expire in one hour. \n\n${verificationLink} \n\nIn case you didn't create an account on whitemind.net, you can safely ignore this email.`,
+          });
+        } catch (e) {
+          req.logger.error(e);
+          return res
+            .status(500)
+            .json({ msg: 'Error sending verification email.' });
+        }
+      }
+
+      user.emails.push({
+        emailType: 'primary',
+        verified: !sendVerification,
+        address: req.body.user.primaryEmail,
+        verificationToken: mailVerificationToken,
+        dateVerificationSent: new Date(),
+      });
+    }
+  }
+
+  if (req.body.user.pronouns !== undefined) {
+    user.pronouns = req.body.user.pronouns;
+  }
+
+  if (req.body.user.company !== undefined) {
+    user.company = req.body.user.company;
+  }
+
+  if (req.body.user.location !== undefined) {
+    user.location = req.body.user.location;
   }
 
   user.set(req.body.user);

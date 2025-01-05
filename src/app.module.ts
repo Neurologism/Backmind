da494aaafeb49ea204b1749539ec53a01b07046d
Emil <me@emilvinu.de>
2025-01-05 22:25:11 +0100

    implement database authentication

--- src/app.module.ts
+++ src/app.module.ts
@@ -15,7 +15,9 @@ import { AppService } from './app.service';
 
 @Module({
   imports: [
-    MongooseModule.forRoot('mongodb://localhost/nest'),
+    MongooseModule.forRoot(process.env.MONGO_URI as string, {
+      dbName: process.env.DB_NAME as string,
+    }),
     AuthModule,
     ProjectsModule,
     TasksModule,

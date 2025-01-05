5f5ffd8fcf0f2186e6712707f5e86c9e14ca627d
Emil <me@emilvinu.de>
2025-01-05 20:04:45 +0100

    add app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,20 +1,26 @@
 import { Module } from '@nestjs/common';
+
+import { MongooseModule } from '@nestjs/mongoose';
+import { AuthModule } from './auth/auth.module';
+import { ProjectsModule } from './projects/projects.module';
+import { TasksModule } from './tasks/tasks.module';
+import { TutorialsModule } from './tutorials/tutorials.module';
+import { UsersModule } from './users/users.module';
+
 import { AppController } from './app.controller';
+
 import { AppService } from './app.service';
-import { UserController } from './user/user.controller';
-import { ProjectController } from './project/project.controller';
-import { TutorialController } from './tutorial/tutorial.controller';
-import { TaskController } from './task/task.controller';
 
 @Module({
-  imports: [],
-  controllers: [
-    AppController,
-    UserController,
-    ProjectController,
-    TutorialController,
-    TaskController,
+  imports: [
+    MongooseModule.forRoot('mongodb://localhost/nest'),
+    AuthModule,
+    ProjectsModule,
+    TasksModule,
+    TutorialsModule,
+    UsersModule,
   ],
+  controllers: [AppController],
   providers: [AppService],
 })
 export class AppModule {}

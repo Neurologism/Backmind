d0f3153d7b10b97dd4e351306ef2a6220947dd9e
Emil <me@emilvinu.de>
2025-01-05 21:49:42 +0100

    implement project controller dto validation

--- src/projects/projects.controller.ts
+++ src/projects/projects.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Post, Res, Req } from '@nestjs/common';
+import { Controller, Post, Res, Req, Body } from '@nestjs/common';
 import { Request, Response } from 'express';
 
 import { createHandler } from './handlers/createHandler';
@@ -8,35 +8,42 @@ import { isTakenHandler } from './handlers/isTakenHandler';
 import { searchHandler } from './handlers/searchHandler';
 import { updateHandler } from './handlers/updateHandler';
 
+import { CreateDto } from './dto/create.schema';
+import { DeleteDto } from './dto/delete.schema';
+import { GetDto } from './dto/get.schema';
+import { IsTakenDto } from './dto/isTaken.schema';
+import { SearchDto } from './dto/search.schema';
+import { UpdateDto } from './dto/update.schema';
+
 @Controller('projects')
 export class ProjectsController {
   @Post('create')
-  create(@Req() req: Request, @Res() res: Response) {
+  create(@Body() body: CreateDto, @Req() req: Request, @Res() res: Response) {
     return createHandler(req, res);
   }
 
   @Post('delete')
-  delete(@Req() req: Request, @Res() res: Response) {
+  delete(@Body() body: DeleteDto, @Req() req: Request, @Res() res: Response) {
     return deleteHandler(req, res);
   }
 
   @Post('get')
-  get(@Req() req: Request, @Res() res: Response) {
+  get(@Body() body: GetDto, @Req() req: Request, @Res() res: Response) {
     return getHandler(req, res);
   }
 
   @Post('is-taken')
-  isTaken(@Req() req: Request, @Res() res: Response) {
+  isTaken(@Body() body: IsTakenDto, @Req() req: Request, @Res() res: Response) {
     return isTakenHandler(req, res);
   }
 
   @Post('search')
-  search(@Req() req: Request, @Res() res: Response) {
+  search(@Body() body: SearchDto, @Req() req: Request, @Res() res: Response) {
     return searchHandler(req, res);
   }
 
   @Post('update')
-  update(@Req() req: Request, @Res() res: Response) {
+  update(@Body() body: UpdateDto, @Req() req: Request, @Res() res: Response) {
     return updateHandler(req, res);
   }
 }

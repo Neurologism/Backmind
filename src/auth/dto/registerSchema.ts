054441968001216b089f94f2adcefe90e50e270f
Emil <me@emilvinu.de>
2025-01-05 17:41:11 +0100

    add auth dtos

---
+++ src/auth/dto/registerSchema.ts
@@ -0,0 +1,30 @@
+import { z } from 'zod';
+import { UserModel } from '../../../mongooseSchemas/user.schema';
+
+export const registerSchema = z
+  .object({
+    user: z
+      .object({
+        email: z.string().email(),
+        brainetTag: z.string().min(Number(process.env.MIN_BRAINET_TAG_LENGTH)),
+        plainPassword: z.string().min(Number(process.env.MIN_PASS_LENGTH)),
+      })
+      .strict(),
+    agreedToTermsOfServiceAndPrivacyPolicy: z.boolean(),
+  })
+  .strict()
+  .refine(
+    async (data) => {
+      return (
+        (await UserModel.findOne({
+          $or: [
+            { 'emails.address': data.user.email },
+            { brainetTag: data.user.brainetTag },
+          ],
+        })) === null
+      );
+    },
+    { message: 'User with that email or brainetTag already exists.' }
+  );
+
+export type RegisterDto = z.infer<typeof registerSchema>;

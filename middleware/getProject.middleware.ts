be619b7bed4712383d2fe1d4730b594e6392cbf4
Emil <me@emilvinu.de>
2025-01-05 20:43:31 +0100

    modify getProject middleware

--- middleware/getProject.middleware.ts
+++ middleware/getProject.middleware.ts
@@ -1,38 +1,38 @@
-import { Response, Request, NextFunction } from 'express';
 import { ProjectModel } from '../mongooseSchemas/project.schema';
+import { Injectable, NestMiddleware } from '@nestjs/common';
+import { Request, Response, NextFunction } from 'express';
 
-export const getProjectMiddleware = async (
-  req: Request,
-  res: Response,
-  next: NextFunction
-) => {
-  const project = await ProjectModel.findById(req.body.project._id);
+@Injectable()
+export class GetProjectMiddleware implements NestMiddleware {
+  async use(req: Request, res: Response, next: NextFunction) {
+    const project = await ProjectModel.findById(req.body.project._id);
 
-  if (project === null) {
-    return res.status(404).json({
-      msg: "This project doesn't exist.",
-    });
-  }
-
-  if (project.visibility === 'private') {
-    if (req.userId === undefined) {
+    if (project === null) {
       return res.status(404).json({
-        msg: 'This project is private. You need to be logged in to access it. ',
+        msg: "This project doesn't exist.",
       });
     }
-    const isOwner = project.ownerId!.toString() === req.userId!.toString();
-    const isContributor = project.contributors
-      .map((contributor) => contributor.toString())
-      .includes(req.userId!.toString());
 
-    if (!isOwner && !isContributor) {
-      return res.status(404).json({
-        msg: 'This project is private. You do not have access to it.',
-      });
+    if (project.visibility === 'private') {
+      if (req.userId === undefined) {
+        return res.status(404).json({
+          msg: 'This project is private. You need to be logged in to access it. ',
+        });
+      }
+      const isOwner = project.ownerId!.toString() === req.userId!.toString();
+      const isContributor = project.contributors
+        .map((contributor) => contributor.toString())
+        .includes(req.userId!.toString());
+
+      if (!isOwner && !isContributor) {
+        return res.status(404).json({
+          msg: 'This project is private. You do not have access to it.',
+        });
+      }
     }
+
+    req.project = project;
+
+    next();
   }
-
-  req.project = project;
-
-  next();
-};
+}

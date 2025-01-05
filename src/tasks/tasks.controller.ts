a218f08754d0d00de85b231ad201cf2b367a0e2c
Emil <me@emilvinu.de>
2025-01-05 22:20:03 +0100

    modify tasks controller

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -2,42 +2,24 @@ import { Controller, Post, Res, Req, Body } from '@nestjs/common';
 import { Request, Response } from 'express';
 
 import { deleteTaskHandler } from './handlers/deleteTaskHandler';
-import { downloadHandler } from './handlers/downloadHandler';
-import { queryHandler } from './handlers/queryHandler';
 import { trainingStartHandler } from './handlers/trainingStartHandler';
 import { trainingStatusHandler } from './handlers/trainingStatusHandler';
 import { trainingStopHandler } from './handlers/trainingStopHandler';
 
 import { DeleteTaskDto } from './dto/deleteTask.schema';
-import { DownloadDto } from './dto/download.schema';
-import { QueryDto } from './dto/query.schema';
 import { TrainingStartDto } from './dto/trainingStart.schema';
 import { TrainingStatusDto } from './dto/trainingStatus.schema';
 import { TrainingStopDto } from './dto/trainingStop.schema';
 
 @Controller('tasks')
 export class TasksController {
-  @Post('download')
-  download(
-    @Body() body: DownloadDto,
-    @Req() req: Request,
-    @Res() res: Response
-  ) {
-    return downloadHandler(req, res);
-  }
-
-  @Post('query')
-  query(@Body() body: QueryDto, @Req() req: Request, @Res() res: Response) {
-    return queryHandler(req, res);
-  }
-
   @Post('training-start')
   trainingStart(
     @Body() body: TrainingStartDto,
     @Req() req: Request,
     @Res() res: Response
   ) {
-    return trainingStartHandler(req, res);
+    return trainingStartHandler(body, req, res);
   }
 
   @Post('training-status')
@@ -46,7 +28,7 @@ export class TasksController {
     @Req() req: Request,
     @Res() res: Response
   ) {
-    return trainingStatusHandler(req, res);
+    return trainingStatusHandler(body, req, res);
   }
 
   @Post('training-stop')
@@ -55,7 +37,7 @@ export class TasksController {
     @Req() req: Request,
     @Res() res: Response
   ) {
-    return trainingStopHandler(req, res);
+    return trainingStopHandler(body, req, res);
   }
 
   @Post('delete-task')
@@ -64,6 +46,6 @@ export class TasksController {
     @Req() req: Request,
     @Res() res: Response
   ) {
-    return deleteTaskHandler(req, res);
+    return deleteTaskHandler(body, req, res);
   }
 }

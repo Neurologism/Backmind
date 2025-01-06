0eb61c122eccd55ff31eb3383c986a32c7c6b173
Emil <me@emilvinu.de>
2025-01-06 09:44:07 +0100

    modify project get handler

--- src/projects/handlers/getHandler.ts
+++ src/projects/handlers/getHandler.ts
@@ -1,6 +1,35 @@
 import { Request, Response } from 'express';
+import { ProjectModel } from 'mongooseSchemas/project.schema';
 
 export const getHandler = async (body: any, req: Request, res: Response) => {
+  const project = await ProjectModel.findById(req.body.project._id);
+
+  if (project === null) {
+    return res.status(404).json({
+      msg: "This project doesn't exist.",
+    });
+  }
+
+  if (project.visibility === 'private') {
+    if (req.userId === undefined) {
+      return res.status(404).json({
+        msg: 'This project is private. You need to be logged in to access it. ',
+      });
+    }
+    const isOwner = project.ownerId!.toString() === req.userId!.toString();
+    const isContributor = project.contributors
+      .map((contributor) => contributor.toString())
+      .includes(req.userId!.toString());
+
+    if (!isOwner && !isContributor) {
+      return res.status(404).json({
+        msg: 'This project is private. You do not have access to it.',
+      });
+    }
+  }
+
+  req.project = project;
+
   req.project!;
   const projectJson = {
     _id: req.project!._id,

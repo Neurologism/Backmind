3f63331c451bd515ab94930b07360c5b3041a731
Servant-of-Scietia <samsun2006@icloud.com>
2025-01-06 20:04:06 +0100

    add logger

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,11 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
+  private readonly logger = new Logger(AppService.name, { timestamp: true });
+
   getHello(): string {
+    this.logger.log('Someone is looking for the root path');
     return "Heyo! You're probably looking for https://whitemind.net";
   }
 }

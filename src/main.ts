4ab56a327ebbc9361965dae69c4599361218841a
Servant-of-Scietia <samsun2006@icloud.com>
2025-01-09 17:46:58 +0100

    fastify migration

--- src/main.ts
+++ src/main.ts
@@ -2,21 +2,17 @@ import { setEnv } from './env';
 import fs from 'fs';
 
 setEnv();
-try {
-  fs.mkdirSync('./logs');
-} catch (err: any) {
-  if (err.code !== 'EEXIST') throw err;
-}
 
 const directories = [
   './logs',
-  process.env.FILES_DIRECTORY as string,
-  process.env.PFP_DIRECTORY as string,
-  process.env.MODEL_DIRECTORY as string,
+  process.env.FILES_DIRECTORY,
+  process.env.PFP_DIRECTORY,
+  process.env.MODEL_DIRECTORY,
 ];
-for (const dir of directories) {
-  if (!fs.existsSync(dir)) fs.mkdirSync(dir, { recursive: true });
-}
+
+directories.forEach((dir) => {
+  if (dir && !fs.existsSync(dir)) fs.mkdirSync(dir, { recursive: true });
+});
 
 import sgMail from '@sendgrid/mail';
 
@@ -25,14 +21,17 @@ sgMail.setApiKey(process.env.SENDGRID_API_KEY as string);
 import { connectToDatabase } from '../utility/connectToDatabase';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { NestExpressApplication } from '@nestjs/platform-express';
-import { AppLogger } from '../providers/logger.provider';
+import {
+  FastifyAdapter,
+  NestFastifyApplication,
+} from '@nestjs/platform-fastify';
 
 async function bootstrap() {
   await connectToDatabase();
-  const app = await NestFactory.create<NestExpressApplication>(AppModule);
-  const appLogger = app.get(AppLogger);
-  app.useLogger(appLogger);
+  const app = await NestFactory.create<NestFastifyApplication>(
+    AppModule,
+    new FastifyAdapter()
+  );
 
   app.enableCors({
     origin:

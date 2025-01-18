46f771510eb9d92b948e70660e5d237c346b7677
Servant-of-Scietia <samsun2006@icloud.com>
2025-01-18 22:15:29 +0100

    fix some issues

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -16,13 +16,11 @@ import { AppLogger } from '../../providers/logger.provider';
 import { AuthService } from './auth.service';
 import { Public } from './strategies/jwt.strategy';
 import { User } from '../../decorators/user.decorator';
+import { UserDocument } from '../../mongooseSchemas/user.schema';
 
 @Controller('auth')
 export class AuthController {
-  constructor(
-    private readonly logger: AppLogger,
-    private readonly authService: AuthService
-  ) {}
+  constructor(private readonly authService: AuthService) {}
 
   @Public()
   @Post('login')
@@ -30,74 +28,45 @@ export class AuthController {
     if (!body.user || (!body.user.brainetTag && !body.user.email)) {
       throw new HttpException('Invalid credentials', HttpStatus.BAD_REQUEST);
     }
-    try {
-      if (body.user.brainetTag) {
-        await this.authService.validateUserByBrainetTag(
-          body.user.brainetTag,
-          body.user.plainPassword
-        );
-      } else if (body.user.email) {
-        await this.authService.validateUserByEmail(
-          body.user.email,
-          body.user.plainPassword
-        );
-      }
-      return await this.authService.login(body.user);
-    } catch (error) {
+    let user: UserDocument;
+    if (body.user.brainetTag) {
+      user = await this.authService.validateUserByBrainetTag(
+        body.user.brainetTag,
+        body.user.plainPassword
+      );
+    } else if (body.user.email) {
+      user = await this.authService.validateUserByEmail(
+        body.user.email,
+        body.user.plainPassword
+      );
+    } else {
       throw new HttpException(
-        (error as any).message,
-        HttpStatus.INTERNAL_SERVER_ERROR
+        'Provide email or brainetTag',
+        HttpStatus.BAD_REQUEST
       );
     }
+    return await this.authService.login(user);
   }
 
   @Public()
   @Post('register')
   async register(@Body() body: RegisterDto) {
-    try {
-      return await this.authService.register(body, this.logger);
-    } catch (error) {
-      throw new HttpException(
-        (error as any).message,
-        HttpStatus.INTERNAL_SERVER_ERROR
-      );
-    }
+    return await this.authService.register(body);
   }
 
   @Delete('logout-all')
-  async logoutAll(@User() user: any) {
-    try {
-      return await this.authService.logoutAll(user);
-    } catch (error) {
-      throw new HttpException(
-        (error as any).message,
-        HttpStatus.INTERNAL_SERVER_ERROR
-      );
-    }
+  async logoutAll(@User() user: UserDocument) {
+    return await this.authService.logoutAll(user);
   }
 
   @Delete('logout')
-  async logout(@Query('token') token: string, @User() user: any) {
-    try {
-      return await this.authService.logout(token, user);
-    } catch (error) {
-      throw new HttpException(
-        (error as any).message,
-        HttpStatus.INTERNAL_SERVER_ERROR
-      );
-    }
+  async logout(@Query('token') token: string, @User() user: UserDocument) {
+    return await this.authService.logout(user, token);
   }
 
   @Public()
   @Patch('verify-email')
   async verifyEmail(@Query('token') token: string) {
-    try {
-      return await this.authService.verifyEmail(token);
-    } catch (error) {
-      throw new HttpException(
-        (error as any).message,
-        HttpStatus.INTERNAL_SERVER_ERROR
-      );
-    }
+    return await this.authService.verifyEmail(token);
   }
 }

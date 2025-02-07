24981ff0e1e3371dff417007daa1ebb5d4e35a4a
lxorb <101118850+lxorb@users.noreply.github.com>
2025-02-07 09:44:48 +0100

    add name to project status return values

--- src/tasks/handlers/trainingStatusHandler.ts
+++ src/tasks/handlers/trainingStatusHandler.ts
@@ -36,6 +36,7 @@ export const trainingStatusHandler = async (
       dateLastUpdated: task.dateLastUpdated,
       projectId: task.projectId,
       ownerId: project.ownerId,
+      name: task.name,
     },
   };
 };
